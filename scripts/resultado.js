2cf4c428d29bf570c3cde303a25315f422a44826
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2020-07-02 12:03:34 -0300

    Add youngest player

--- scripts/resultado.js
+++ scripts/resultado.js
@@ -1,12 +1,66 @@
 (async () => {
-  const resultados = await fetch('./vencedores.json')
-  console.log(resultados)
+  const resultados = await fetch('./vencedores.json').then(res => res.json())
+  
+  const jogosVencedores = document.querySelectorAll('[data-vencedor]')
+  const jogosMaisNovos = document.querySelectorAll('[data-mais-novo]')
+  
+  carregaJogos('vencedores', jogosVencedores, resultados)
+  carregaJogos('maisNovos', jogosMaisNovos, resultados)
 })()
 
-const Jogo = (props) => {
-  const jogo = document.createElement('div')
+const carregaJogos = (categoria, elementos, resultados) => {
+  elementos.forEach((jogo, indice) => {
+    const dadosJogo = resultados[categoria][indice]
+    jogo.appendChild(Jogo(dadosJogo, categoria, indice))
+  })
+}
 
-  jogo.innerHTML = '<h1>Teste</h1>'
+const Jogo = ({
+  nome, 
+  idade,
+  colocacao,
+  nomeDoJogo,
+  lugarDuplicado,
+  url
+}, categoria, indice) => {
+  const jogo = document.createElement('a')
+  jogo.classList.add('podio')
+  jogo.classList.add(`podio-${colocacao}`)
+  jogo.classList.add('vencedor')
+  jogo.setAttribute('href', url)
+  jogo.setAttribute('target', '_blank')
+
+  const indiceImagem = colocacao ? colocacao : indice
+
+  const imagem = lugarDuplicado ? `${indiceImagem}3` : indiceImagem
+
+  const colocacaoHTML = colocacao ? `    <div class="podio-colocacao colocacao">
+      ${colocacao}
+    </div> ` : ''
+
+  jogo.innerHTML = 
+  `
+    <div class="imagem-container">
+      <img
+        src="./assets/${categoria}/${imagem}.png" 
+        alt="imagem do jogo do vencedor" 
+        class="vencedor-imagem"
+      >
+    </div>
+
+    <div class="vencedor-conteudo">
+      <h2 class="jogo-titulo">${nomeDoJogo}</h2>
+      <p>${nome}</p>
+      <p>${idade} anos</p>
+    </div>
+    
+    ${colocacaoHTML}
+  `
 
   return jogo
+}
+
+const toggleModal = (codigoModal) => {
+  const modal = document.querySelector(`[data-modal="${codigoModal}"]`)
+  modal.classList.add('modal-show')
 }
